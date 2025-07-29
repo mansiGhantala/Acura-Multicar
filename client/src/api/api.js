33bcfacbef01dde72f83bacfc6d2b164bed15208
mansiGhantala <mansighantala@gmailcom>
2025-07-29 21:56:44 +0530

    card related deploy

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -4,3 +4,13 @@ const api = axios.create({
   baseURL: "https://acura-multicar.onrender.com/api",
   withCredentials: true,
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -30,7 +30,7 @@ const upload = multer({
   storage,
   fileFilter,
   limits: {
-    fileSize: 5 * 1024 * 1024,
+    fileSize: 5 * 1024 * 1024, // 5MB
   },
 });
 
