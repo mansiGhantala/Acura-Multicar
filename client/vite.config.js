79f37300fa39ed66004e23fb66be5d68e178601a
mansiGhantala <mansighantala@gmailcom>
2025-07-29 21:03:17 +0530

    final deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import home from "@vitejs/plugin-react"
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [home(),    tailwindcss(),
-],
-})
+  plugins: [react(), tailwindcss()],
+  base: '/', 
+  server: {
+    port: 5173,
+  },
+});
